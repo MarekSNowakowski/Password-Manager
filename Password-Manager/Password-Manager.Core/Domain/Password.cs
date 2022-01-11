ded2a55c0b9ca87a04ea0eb01a31d5dff3814ee8
Marek Nowakowski <marsznow@gmail.com>
2022-01-11 20:48:12 +0100

    Created solution and Core project

---
+++ Password-Manager/Password-Manager.Core/Domain/Password.cs
@@ -0,0 +1,9 @@
+﻿namespace Password_Manager.Core.Domain
+{
+    public class Password
+    {
+        public int Id { get; set; }
+        public string Service { get; set; }
+        public string Pass { get; set; }
+    }
+}
---
+++ Password-Manager/Password-Manager.Core/Repositories/IPasswordRepository.cs
@@ -0,0 +1,17 @@
+﻿using Password_Manager.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Password_Manager.Core.Repositories
+{
+    public interface IPasswordRepository
+    {
+        public Task<IEnumerable<Password>> BrowseAllAsync();
+        public Task<Password> GetAsync(int id);
+        public Task AddAsync(Password s);
+        public Task DelAsync(int id);
+        public Task UpdateAsync(Password s);
+    }
+}
